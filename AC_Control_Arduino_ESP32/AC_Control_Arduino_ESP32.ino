616c486b694837c09aad56294c765ba60981e86f
wastman <37987668+wastman@users.noreply.github.com>
2018-04-11 21:44:17 +0200

    Adding_MQTT_Support
    Adding mqtt client to ESP32, SPI.h class gets explicit included - just to make sure the compiler will include it

--- AC_Control_Arduino_ESP32/AC_Control_Arduino_ESP32.ino
+++ AC_Control_Arduino_ESP32/AC_Control_Arduino_ESP32.ino
@@ -1,6 +1,8 @@
 #include <DHT.h>
+#include <SPI.h>
 #include <MFRC522.h>
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 //LEDs and control for AC heating/cooling control
 #define COOL_AC_BLUE_LED 25
@@ -27,9 +29,16 @@ MFRC522 rfidSensor(SS_PIN, RST_PIN);
 const char* ssid = "WlanWastman";
 const char* password = "kreuzer1908";
 const char* mqtt_server = "192.168.119.44";
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+//Define mqtt topics
+#define STATUS_AC_TOPIC    "AcControl/Status"
+#define MODE_TOPIC    "AcControl/Mode"
 
 //global variables
 boolean updateMeasurement = false;
+char msg[20];
 
 void setup() {
 //initial LEDs and control for AC heating/cooling control
@@ -58,6 +67,11 @@ timerAlarmEnable(updateTimer);
 
 //connecting with wifi
 wificonnect();
+
+//connecting with mqtt
+client.setServer(mqtt_server, 1883);//MQTT server with IPaddress,port
+client.setCallback(receivedCallback);//receivedCallback function invoked client received subscribed topic
+mqttconnect();
   
 }
 
@@ -65,6 +79,11 @@ void loop() {
 //reconnect wifi if not connected
 if (WiFi.status() != WL_CONNECTED){
   wificonnect();
+}
+
+//reconnect mqtt if not connected
+if (!client.connected()) {
+  mqttconnect();
 }
 
 //Every 0,5 seconds update measurement
@@ -75,6 +94,8 @@ if (updateMeasurement == true){
 //Check for RFID tag
 CheckForRFIDCard();
 
+//Check for mqtt msg
+client.loop();
 }
 
 void UpdateMeasurement() {
@@ -113,6 +134,19 @@ Serial.println(rfidIDString);
  } 
 }
 
+//receiving mqtt msg
+void receivedCallback(char* topic, byte* payload, unsigned int length) {
+  String recMsg = "";
+  Serial.print("Message received: ");
+  Serial.println(topic);
+
+//Hier kommt die nachricht rein
+  Serial.print("payload: ");
+  for (int i = 0; i < length; i++) {
+    recMsg = recMsg + (char)payload[i];
+  }
+} 
+
 void wificonnect() {
 Serial.print("Connecting to ");
 Serial.println(ssid);
@@ -125,6 +159,23 @@ Serial.println("");
 Serial.println("WiFi connected");
 Serial.println("IP address: ");
 Serial.println(WiFi.localIP());  
+}
+
+void mqttconnect() {
+  while (!client.connected()) {
+    Serial.print("MQTT connecting ...");
+    String clientId = "AcControl";
+    //connect now
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      client.subscribe(MODE_TOPIC);
+    } else {
+      Serial.print("failed, status code =");
+      Serial.print(client.state());
+      Serial.println("try again in 3 seconds");
+      delay(3000);
+    }
+  }
 }
 
 void UpdateTimerInterrupt(){
