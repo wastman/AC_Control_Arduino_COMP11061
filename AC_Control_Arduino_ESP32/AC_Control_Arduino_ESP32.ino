7c55b40bee4e1e95a467bcb9b7069a5e78cee4e7
wastman <37987668+wastman@users.noreply.github.com>
2018-04-11 12:53:24 +0200

    Adding LED support
    Configuartion of output pins, defining names for pins

--- AC_Control_Arduino_ESP32/AC_Control_Arduino_ESP32.ino
+++ AC_Control_Arduino_ESP32/AC_Control_Arduino_ESP32.ino
@@ -1,6 +1,13 @@
+//LEDs and control for AC heating/cooling control
+#define COOL_AC_BLUE_LED 25
+#define HEAT_AC_RED_LED 26
+
 void setup() {
   // put your setup code here, to run once:
-
+//initial LEDs and control for AC heating/cooling control
+pinMode(COOL_AC_BLUE_LED, OUTPUT);//configurate output
+pinMode(HEAT_AC_RED_LED, OUTPUT);//configurate output
+  
 }
 
 void loop() {
